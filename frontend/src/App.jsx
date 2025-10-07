bbc985a52bb7891cb321d5bf3b5254f424c9335f
Marius YVART <mariusyvt@gmail.com>
2025-10-07 16:33:10 +0200

    edit name

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,8 +7,8 @@ import {
 import { AuthProvider } from "./context/AuthContext";
 import PrivateRoute from "./components/Layout/PrivateRoute";
 import Navbar from "./components/Layout/Navbar";
-import Login from "./components/Auth/login";
-import Register from "./components/Auth/register";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
 import ContactList from "./components/Contacts/ContactList";
 
 function App() {
