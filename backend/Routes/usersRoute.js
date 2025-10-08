cd304e2814830a5f27120fe7bffe523dbff03b36
Marius YVART <mariusyvt@gmail.com>
2025-10-08 19:09:12 +0200

    Git

--- backend/Routes/usersRoute.js
+++ backend/Routes/usersRoute.js
@@ -1,11 +1,9 @@
 const express = require("express");
 
 const usersController = require("../Controllers/usersController");
-// const contactController = require("../Controllers/contactsController");
 
 const router = express.Router();
 
 router.get("/getUsers", usersController);
-// router.get("/getContacts", contactController);
 
 module.exports = router;
